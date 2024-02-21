1b312d8a3cb3e988eaabe9c15b28ec55d1200f13
Cuk-o <35926436+Cuk-o@users.noreply.github.com>
2024-02-21 09:13:20 +0900

    Update tgbot.py
    добавлен прогрессбар
    фикс мелких ошибок

--- tgbot.py
+++ tgbot.py
@@ -1,99 +1,17 @@
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import logging
 import csv
 import re
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-currency_rates = {
-    "DZD": 134.55, "AUD": 1.53, "BHD": 0.376, "BDT": 109.73, "BOB": 6.91,
-    "BRL": 4.97,  "CAD": 1.35, "KYD": 0.833, "CLP": 970.01,
-    "COP": 3902.4, "CRC": 515.06, "EGP": 30.9, "GEL": 2.64, "GHS": 12.49,
-    "HKD": 7.82, "INR": 83.05, "IDR": 15646.73, "IQD": 1309.63, "ILS": 3.61,
-    "JPY": 150.2, "JOD": 0.709, "KZT": 450.04, "KES": 146.9, "KRW": 1332.97,
-    "KWD": 0.308, "MOP": 8.06, "MYR": 4.78, "MXN": 17.06, "MAD": 10.06,
-    "MMK": 2094.53, "NZD": 1.63, "NGN": 1505.32, "OMR": 0.384, "PKR": 279.29,
-    "PYG": 7298.36, "PEN": 3.84, "PHP": 55.94, "QAR": 3.64, "RUB": 92.12,
-    "SAR": 3.75, "RSD": 108.77, "SGD": 1.35, "ZAR": 18.88, "LKR": 312.29,
-    "TWD": 31.33, "TZS": 2541.15, "THB": 36, "TRY": 30.87, "UAH": 37.96,
-    "AED": 3.67, "USD": 1, "VND": 24526.38
-}
-
-
-# Настройка логирования
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-def convert_price_to_usd(price_str, currency_code):
-    price_match = re.search(r"(\d[\d,.]*)", price_str.replace(",", ""))
-    if price_match:
-        price = float(price_match.group(1).replace(",", ""))
-        rate = currency_rates.get(currency_code.upper(), 1)  # По умолчанию 1, если валюта не найдена
-        if currency_code == 'IDR':
-            return round(price / rate / 100, 2)
-        else:    
-            return round(price / rate, 2)  # Конвертация в USD и округление
-    return 0
-
-def get_prices_for_country(driver, country_code, country_currency_dict, app_id):
-    """
-    Получает цены для страны по указанному идентификатору приложения.
-
-    :param driver: Драйвер браузера.
-    :param country_code: Код страны.
-    :param country_currency_dict: Словарь с соответствием кода страны и валюты.
-    :param app_id: Идентификатор приложения на Google Play.
-    :return: Список найденных цен и код валюты.
-    """
-    # Получаем код валюты для страны, по умолчанию USD
-    currency_code = country_currency_dict.get(country_code, "USD")
-    
-    # Формируем URL для доступа к странице приложения в Google Play
-    url = f'https://play.google.com/store/apps/details?id={app_id}&hl=en_US&gl={country_code}'
-    
-    # Открываем страницу приложения
-    driver.get(url)
-    print(country_code)
-    # Ждем 2 секунды, чтобы страница полностью загрузилась
-    time.sleep(2)
-    
-    # Прокручиваем страницу до конца
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    
-    # Ждем 1 секунду после прокрутки
-    time.sleep(1)
-    
-    # Получаем исходный код страницы
-    page_source = driver.page_source
-    
-    # Используем регулярное выражение для поиска цен
-    regex_pattern = r'"([^"]*?\sper\sitem)",'
-    matches = re.findall(regex_pattern, page_source)
-    
-    return matches, currency_code
-
-
-# Функции для работы с WebDriver
-def setup_driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--log-level=3")
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    return driver
-
-# Команда start
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Отправь мне идентификатор приложения на Google Play, например com.habby.archero')
-
-# Обработка текстовых сообщений
-def handle_message(update: Update, context: CallbackContext) -> None:
-    app_id = update.message.text
-    country_currency_dict = {
+   
+country_currency_dict = {
     "DZ": "DZD",
     "AU": "AUD",
     "BH": "BHD",
@@ -151,35 +69,152 @@ def handle_message(update: Update, context: CallbackContext) -> None:
 }
 
 
-    countries = ["DZ", "AU", "BH", "BD", "BO", "BR", "CA", "KY", "CL", "CO", "CR", 
-                 "SV", "GE", "GH", "HK", "IN", "ID", "IQ", "IL", "JP", "JO", "KZ", "KE", "KR", 
-                 "KW", "MO", "MY", "MX", "MA", "MM", "NZ", "NG", "OM", "PK", "PY", "PE", "PH", "QA", 
-                 "RU", "SA", "RS", "SG", "ZA", "LK", "TW", "TZ", "TH", "TR", "UA", "AE", "US", "VN"]
+countries = ["DZ", "AU", "BH", "BD", "BO", "BR", "CA", "KY", "CL", "CO", "CR", 
+            "SV", "GE", "GH", "HK", "IN", "ID", "IQ", "IL", "JP", "JO", "KZ", "KE", "KR", 
+            "KW", "MO", "MY", "MX", "MA", "MM", "NZ", "NG", "OM", "PK", "PY", "PE", "PH", "QA", 
+            "RU", "SA", "RS", "SG", "ZA", "LK", "TW", "TZ", "TH", "TR", "UA", "AE", "US", "VN"]
 
+
+currency_rates = {
+    "DZD": 134.55, "AUD": 1.53, "BHD": 0.376, "BDT": 109.73, "BOB": 6.91,
+    "BRL": 4.97,  "CAD": 1.35, "KYD": 0.833, "CLP": 970.01,
+    "COP": 3902.4, "CRC": 515.06, "EGP": 30.9, "GEL": 2.64, "GHS": 12.49,
+    "HKD": 7.82, "INR": 83.05, "IDR": 15646.73, "IQD": 1309.63, "ILS": 3.61,
+    "JPY": 150.2, "JOD": 0.709, "KZT": 450.04, "KES": 146.9, "KRW": 1332.97,
+    "KWD": 0.308, "MOP": 8.06, "MYR": 4.78, "MXN": 17.06, "MAD": 10.06,
+    "MMK": 2094.53, "NZD": 1.63, "NGN": 1505.32, "OMR": 0.384, "PKR": 279.29,
+    "PYG": 7298.36, "PEN": 3.84, "PHP": 55.94, "QAR": 3.64, "RUB": 92.12,
+    "SAR": 3.75, "RSD": 108.77, "SGD": 1.35, "ZAR": 18.88, "LKR": 312.29,
+    "TWD": 31.33, "TZS": 2541.15, "THB": 36, "TRY": 30.87, "UAH": 37.96,
+    "AED": 3.67, "USD": 1, "VND": 24526.38
+}
+
+
+# Настройка логирования
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def convert_price_to_usd(price_str, currency_code):
+    price_match = re.search(r"(\d[\d,.]*)", price_str.replace(",", ""))
+    if price_match:
+        price = float(price_match.group(1).replace(",", ""))
+        rate = currency_rates.get(currency_code.upper(), 1)  # По умолчанию 1, если валюта не найдена
+        if currency_code == 'IDR':
+            return round(price / rate / 100, 2)
+        else:    
+            return round(price / rate, 2)  # Конвертация в USD и округление
+    return 0
+
+def get_prices_for_country(driver, country_code, country_currency_dict, app_id):
+   
+    # Получаем код валюты для страны, по умолчанию USD
+    currency_code = country_currency_dict.get(country_code, "USD")
+    
+    # Формируем URL для доступа к странице приложения в Google Play
+    url = f'https://play.google.com/store/apps/details?id={app_id}&hl=en_US&gl={country_code}'
+    
+    # Открываем страницу приложения
+    driver.get(url)
+    print(country_code)
+    # Ждем 2 секунды, чтобы страница полностью загрузилась
+    time.sleep(1)
+    
+    # Прокручиваем страницу до конца
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    
+    # Ждем 1 секунду после прокрутки
+    time.sleep(1)
+    
+    # Получаем исходный код страницы
+    page_source = driver.page_source
+    
+    # Используем регулярное выражение для поиска цен
+    regex_pattern = r'"([^"]*?\sper\sitem)",'
+    matches = re.findall(regex_pattern, page_source)
+    
+    return matches, currency_code
+
+
+# Функции для работы с WebDriver
+def setup_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--log-level=3")
+    chrome_options.add_argument("--no-sandbox")
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    return driver
+
+# Команда start
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Привет! Отправь мне идентификатор приложения на Google Play, например com.habby.archero')
+
+# Обработка текстовых сообщений
+
+def handle_message(update: Update, context: CallbackContext) -> None:
+    app_id = update.message.text
+    directory_path = "/root/googleplay"  # Укажите ваш путь к директории для сохранения файлов
+    filename = f"{app_id}.csv"
+    filepath = os.path.join(directory_path, filename)
+
+
+    progress_message = update.message.reply_text('Обработка началась. Прогресс: 0%')
+    progress_message_id = progress_message.message_id
+    progress_chat_id = progress_message.chat_id
 
     driver = setup_driver()
+    processed_countries = 0
+    countries_total = len(countries)
+
+
+
+    # Проверка на существование файла и его размер
+    if os.path.exists(filepath):
+        file_size = os.path.getsize(filepath)
+        if file_size == 1939:
+            context.bot.edit_message_text(chat_id=progress_chat_id, message_id=progress_message_id, text='У этого приложения нет внутриигровых покупок.')
+
+            #update.message.reply_text('У этого приложения нет внутриигровых покупок.')
+            return
+        elif file_size < 1939:
+            os.remove(filepath)  # Удаляем файл
+            #update.message.reply_text('Провожу повторную проверку...')
+            # Здесь код для повторной генерации файла или получения данных
+        else:
+            context.bot.edit_message_text(chat_id=progress_chat_id, message_id=progress_message_id, text="Обработка завершена.")
+
+            #update.message.reply_text('Файл уже существует, отправляю сохраненный файл...')
+            with open(filepath, 'rb') as file:
+                update.message.reply_document(document=file)
+            return
+
+    # Если файла нет или он был удален из-за малого размера, начинаем процесс создания файла
+   
+    driver = setup_driver()
     try:
-        # Измененное имя файла для отражения содержания
-        with open('prices_by_country_converted.csv', mode='w', newline='', encoding='utf-8') as file:
+        with open(filepath, mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerow(['Country', 'Currency', 'Original Price Range', 'Converted Price Range (USD)'])
-
+            #update.message.reply_text('Среднее время ответа 2 минуты')
             for country_code in countries:
+                processed_countries += 1
+                percent_done = (processed_countries / countries_total) * 100
+                text = f'Прогресс: {percent_done:.2f}%'
+                context.bot.edit_message_text(chat_id=progress_chat_id, message_id=progress_message_id, text=text)
+                
+                # Задержка, чтобы избежать чрезмерного количества запросов
+                time.sleep(0.5)
                 prices, currency_code = get_prices_for_country(driver, country_code, country_currency_dict, app_id)
                 converted_prices_text = []
                 if prices:
                     for price_str in prices:
-                        # Извлечение числовых значений из строки цен
                         price_values = re.findall(r'\d[\d,.]*', price_str)
                         for value in price_values:
-                            # Удаление разделителей тысяч для корректной конвертации
                             if country_code == 'ID':
                                 value = value.replace(",", ".")
                                 value = value.replace(".", "")
-
                             else:    
                                 value = value.replace(",", "")
-                            # Предполагается, что функция convert_price_to_usd уже реализована
                             converted_price = convert_price_to_usd(value, currency_code)
                             converted_prices_text.append(f"{converted_price} USD")
                 else:
@@ -187,15 +222,34 @@ def handle_message(update: Update, context: CallbackContext) -> None:
 
                 writer.writerow([country_code, currency_code, '; '.join(prices), '; '.join(converted_prices_text)])
 
+        #update.message.reply_text('Обработка завершена, отправляю файл...')
+        with open(filepath, 'rb') as file:
+            update.message.reply_document(document=file)
+    finally:
+        context.bot.edit_message_text(chat_id=progress_chat_id, message_id=progress_message_id, text="Обработка завершена.")
+        driver.quit()
+        
+
+# Дополните этот код своими функциями setup_driver, convert_price_to_usd и get_prices_for_country
+
+
+def process_and_create_file(update: Update, context: CallbackContext, app_id: str, filepath: str):
+    driver = setup_driver()
+    try:
+        with open(filepath, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Country', 'Currency', 'Original Price Range', 'Converted Price Range (USD)'])
+            # Здесь продолжается логика обработки, как в исходном примере...
+        
+        # После создания файла, отправляем его
         update.message.reply_text('Обработка завершена, отправляю файл...')
-        # Отправка файла
-        update.message.reply_document(document=open('prices_by_country_converted.csv', 'rb'))
+        update.message.reply_document(document=open(filepath, 'rb'))
     finally:
         driver.quit()
 
 def main():
     # Токен вашего бота
-    TOKEN = 'telegram_bot_token'
+    TOKEN = ''
 
     updater = Updater(TOKEN)
 
@@ -210,3 +264,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
